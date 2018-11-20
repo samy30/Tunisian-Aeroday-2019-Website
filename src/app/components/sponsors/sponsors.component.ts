e3843ae2df924f341033adae35c61a4719c0ec32
samy30 <samibelaid30@gmail.com>
2018-11-20 10:44:40 +0100

    add sponsors

--- src/app/components/sponsors/sponsors.component.ts
+++ src/app/components/sponsors/sponsors.component.ts
@@ -10,6 +10,26 @@ export class SponsorsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      $('#myCarousel').carousel({
+          interval: 4000
+      })
+
+      $('.carousel .item').each(function(){
+          var next = $(this).next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
+          }
+          next.children(':first-child').clone().appendTo($(this));
+
+          for (var i=0;i<4;i++) {
+              next=next.next();
+              if (!next.length) {
+                  next = $(this).siblings(':first');
+              }
+
+              next.children(':first-child').clone().appendTo($(this));
+          }
+      });
   }
 
 }
